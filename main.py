6131d06163b113c09caef09b328b10040d1f9cc9
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2021-03-14 01:21:48 +0200

    model logistic regression

--- main.py
+++ main.py
@@ -6,7 +6,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+
+
+class Model:
+    def __init__(self):
+        self.logit_regressor = LogisticRegression(random_state=42)
+        self.data_loader = DataLoader()
+        self.model = None
+
+    def run(self):
+        x_train_vec, y_train, x_val_vec, y_val, x_test_vec, y_test = self.data_loader.run()
+        print("Model Training...")
+        self.model = self.logit_regressor.fit(x_train_vec, y_train)
+        print(classification_report(y_val, self.model.predict(x_val_vec)))
 
 
 class DataLoader:
@@ -14,6 +30,7 @@ class DataLoader:
         self.train_df = pd.read_csv(train_path, encoding='latin-1')
         self.test_df = pd.read_csv(test_path, encoding='latin-1')
         self.stopWord = stopwords.words('english')
+        self.covid_corpus = None
 
     def visualize_labels(self):
         labels = ['Positive', 'Negative', 'Neutral', 'Extremely Positive', 'Extremely Negative']
@@ -28,6 +45,7 @@ class DataLoader:
         plt.show()
 
     def run(self):
+        print("DataLoader running...")
         train, test = self.preprocess()
         x_train = train.Corpus
         y_train = train.Sentiment
@@ -35,8 +53,16 @@ class DataLoader:
         y_test = test.Sentiment
 
         x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
-        print("Train Shape...\nX: {}\nY: {}\nValidation Shape...\nX: {}\nY: {}\nTest Shape...\nX: {}\n Y: {}\n".format(
+        print("Train Shape...\nX: {}\nY: {}\nValidation Shape...\nX: {}\nY: {}\nTest Shape...\nX: {}\nY: {}\n".format(
             x_train.shape, y_train.shape, x_val.shape, y_val.shape, x_test.shape, y_test.shape))
+
+        vectorizer = CountVectorizer(stop_words='english', ngram_range=(1, 2), min_df=5).fit(self.covid_corpus)
+
+        x_train_vec = vectorizer.transform(x_train)
+        x_val_vec = vectorizer.transform(x_val)
+        x_test_vec = vectorizer.transform(x_test)
+
+        return x_train_vec, y_train, x_val_vec, y_val, x_test_vec, y_test
 
     def preprocess(self):
         self.train_df['Identity'] = 0
@@ -69,6 +95,8 @@ class DataLoader:
         covid.Corpus = covid.apply(lambda x: [lemma.lemmatize(word) for word in x.Corpus], axis=1)
         covid.Corpus = covid.apply(lambda x: " ".join(x.Corpus), axis=1)
 
+        self.covid_corpus = covid.Corpus
+
         train = covid[covid.Identity == 0]
         test = covid[covid.Identity == 1]
         train.drop('Identity', axis=1, inplace=True)
@@ -89,5 +117,5 @@ class DataLoader:
         return text
 
 
-model = DataLoader()
-model.preprocess()
+model = Model()
+model.run()
